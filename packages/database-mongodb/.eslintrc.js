5022f19b1c63c0a2136aec5a99b76e96d94f508c
Munif Tanjim <muniftanjim@gmail.com>
2020-03-03 23:28:58 +0600

    chore(database-mongodb): update ESLint configuration

--- packages/database-mongodb/.eslintrc.js
+++ packages/database-mongodb/.eslintrc.js
@@ -1,17 +1,11 @@
 module.exports = {
-  extends: [
-    'standard-with-typescript',
-    'plugin:prettier/recommended',
-    'prettier/standard',
-    'prettier/@typescript-eslint'
-  ],
+  extends: ['@synor/eslint-config'],
   parserOptions: {
     project: './tsconfig.json',
     createDefaultProgram: true
   },
   rules: {
-    semi: ['error', 'always'],
-    '@typescript-eslint/strict-boolean-expressions': 'off'
+    semi: ['error', 'always']
   },
   plugins: ['jest'],
   env: {
