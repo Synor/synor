41547808d5300e889255c266a98247999e0cb22b
Munif Tanjim <muniftanjim@gmail.com>
2020-03-06 12:36:22 +0600

    test(database-mongodb): setup test environment

--- packages/database-mongodb/jest.config.js
+++ packages/database-mongodb/jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  testEnvironment: 'node',
+  testEnvironment: './jest.env.js',
   testPathIgnorePatterns: ['/node_modules/', '__utils__\\.test\\.[jt]s$']
 };
---
+++ packages/database-mongodb/jest.env.js
@@ -0,0 +1,59 @@
+const NodeEnvironment = require('jest-environment-node');
+const { MongoClient } = require('mongodb');
+
+const user = 'mongo';
+const password = 'mongo';
+const database = 'synor';
+const uri = `mongodb://${user}:${password}@127.0.0.1:27017`;
+
+const sleep = async ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const connectToMongoDB = async () => {
+  const client = await MongoClient.connect(uri, { poolSize: 1 });
+  const db = client.db(database);
+  return { client, db };
+};
+
+async function waitForMongoDB() {
+  try {
+    const { client, db } = await connectToMongoDB();
+
+    await db
+      .command({ usersInfo: { user, db: database } })
+      .then(({ users }) => {
+        const userExists = users.length;
+        if (!userExists) {
+          return db.addUser(user, password, {
+            roles: [{ role: 'dbOwner', db: database }]
+          });
+        }
+      });
+
+    await client.close();
+  } catch (_) {
+    await sleep(1000);
+    return waitForMongoDB();
+  }
+}
+
+class SynorDatabaseMongoDBTestEnvironment extends NodeEnvironment {
+  constructor(config, context) {
+    super(config, context);
+    this.docblockPragmas = context.docblockPragmas;
+  }
+
+  async setup() {
+    await super.setup();
+    await waitForMongoDB();
+  }
+
+  async teardown() {
+    await super.teardown();
+  }
+
+  runScript(script) {
+    return super.runScript(script);
+  }
+}
+
+module.exports = SynorDatabaseMongoDBTestEnvironment;
