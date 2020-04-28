233ac3c99e807f4074bdbd12c8a1f8719e6e2ec2
Munif Tanjim <muniftanjim@gmail.com>
2020-04-28 23:45:54 +0600

    ci: tweak scripts/ci/checkout.sh

--- scripts/ci/checkout.sh
+++ scripts/ci/checkout.sh
@@ -8,6 +8,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 : "${CI_TOOL:=github}"
 : "${CI_PLUGIN:=$DIR/plugins/${CI_TOOL}.sh}"
 
+MAX_DEPTH=500
+
 if [[ $GITHUB_EVENT_NAME == "push" ]]; then
   LAST_SUCCESSFUL_SHA=$(${CI_PLUGIN} sha last)
   echo "Last successful commit: ${LAST_SUCCESSFUL_SHA}"
@@ -21,9 +23,14 @@ if [[ $GITHUB_EVENT_NAME == "push" ]]; then
       DEPTH=1
       until git show "${LAST_SUCCESSFUL_SHA}" > /dev/null 2>&1
       do
-        DEPTH=$((DEPTH+5))
+        DEPTH=$((DEPTH+10))
         echo "Last successful commit not fetched yet. Fetching depth $DEPTH."
         git fetch --depth=$DEPTH
+
+        if [[ "${DEPTH}" -gt "${MAX_DEPTH}" ]]; then
+          echo "Reached max depth ${MAX_DEPTH}. Exiting..."
+          exit 0
+        fi
       done
 
       ${CI_PLUGIN} set-env "LAST_SUCCESSFUL_SHA" "${LAST_SUCCESSFUL_SHA}"
