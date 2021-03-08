17c5e37f76749ed47f531352dafa6669c3595595
Munif Tanjim <muniftanjim@gmail.com>
2021-03-09 02:19:15 +0600

    perf(cli): improve .ts config file reading performance
    disable type checking when reading .ts config using ts-node

--- packages/cli/src/utils/register-ts-node.ts
+++ packages/cli/src/utils/register-ts-node.ts
@@ -2,5 +2,5 @@ type TSNode = typeof import('ts-node')
 
 export function registerTSNode() {
   const tsNode: TSNode = require('ts-node')
-  tsNode.register({})
+  tsNode.register({ transpileOnly: true })
 }
