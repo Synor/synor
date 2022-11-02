892aafd11c7f18937c8f60c2dd9a203b89d03bc5
Munif Tanjim <hello@muniftanjim.dev>
2022-11-02 20:00:08 +0600

    test(database-mysql): add tests for MySQL8

--- packages/database-mysql/src/index.test.ts
+++ packages/database-mysql/src/index.test.ts
@@ -66,7 +66,10 @@ const getUserInfo: GetUserInfo = () => Promise.resolve(`Jest`)
 const databaseName = 'synor'
 const tableName = 'test_record'
 const params = `synor_migration_record_table=${tableName}`
-const uri = `mysql://root:root@127.0.0.1:3306/${databaseName}?${params}`
+const uris = {
+  mysql5: `mysql://root:root@127.0.0.1:33065/${databaseName}?${params}`,
+  mysql8: `mysql://root:root@127.0.0.1:33068/${databaseName}?${params}`,
+}
 
 describe('module exports', () => {
   test('default export exists', () => {
@@ -82,324 +85,334 @@ describe('module exports', () => {
   })
 })
 
-describe('initialization', () => {
-  let dbUri: Parameters<typeof MySQLDatabaseEngine>[0]
-  const helpers: Parameters<typeof MySQLDatabaseEngine>[1] = {
-    baseVersion,
-    getAdvisoryLockId,
-    getUserInfo,
-  }
+describe.each(Object.entries(uris))('%s', (_, uri) => {
+  describe('initialization', () => {
+    let dbUri: Parameters<typeof MySQLDatabaseEngine>[0]
+    const helpers: Parameters<typeof MySQLDatabaseEngine>[1] = {
+      baseVersion,
+      getAdvisoryLockId,
+      getUserInfo,
+    }
 
-  beforeEach(() => {
-    dbUri = uri
-    helpers.baseVersion = baseVersion
-    helpers.getAdvisoryLockId = getAdvisoryLockId
-    helpers.getUserInfo = getUserInfo
-  })
-
-  test.each([undefined, null, 0])('throws if uri is %s', (uri) => {
-    expect(() => MySQLDatabaseEngine(uri as any, helpers)).toThrow()
-  })
-
-  test('throws if uri is empty', () => {
-    dbUri = ' '
-    expect(() => MySQLDatabaseEngine(dbUri, helpers)).toThrow()
-  })
-
-  describe('helpers validation', () => {
     beforeEach(() => {
+      dbUri = uri
+      helpers.baseVersion = baseVersion
       helpers.getAdvisoryLockId = getAdvisoryLockId
       helpers.getUserInfo = getUserInfo
     })
 
-    test(`throws if getAdvisoryLockId is missing`, () => {
-      delete helpers.getAdvisoryLockId
+    test.each([undefined, null, 0])('throws if uri is %s', (uri) => {
+      expect(() => MySQLDatabaseEngine(uri as any, helpers)).toThrow()
+    })
+
+    test('throws if uri is empty', () => {
+      dbUri = ' '
       expect(() => MySQLDatabaseEngine(dbUri, helpers)).toThrow()
     })
 
-    test(`throws if getAdvisoryLockId is not function`, () => {
-      helpers.getAdvisoryLockId = '' as any
-      expect(() => MySQLDatabaseEngine(dbUri, helpers)).toThrow()
-      helpers.getAdvisoryLockId = null as any
-      expect(() => MySQLDatabaseEngine(dbUri, helpers)).toThrow()
-    })
+    describe('helpers validation', () => {
+      beforeEach(() => {
+        helpers.getAdvisoryLockId = getAdvisoryLockId
+        helpers.getUserInfo = getUserInfo
+      })
 
-    test(`throws if getUserInfo is missing`, () => {
-      delete helpers.getUserInfo
-      expect(() => MySQLDatabaseEngine(dbUri, helpers)).toThrow()
-    })
+      test(`throws if getAdvisoryLockId is missing`, () => {
+        delete helpers.getAdvisoryLockId
+        expect(() => MySQLDatabaseEngine(dbUri, helpers)).toThrow()
+      })
 
-    test(`throws if getUserInfo is not function`, () => {
-      helpers.getUserInfo = '' as any
-      expect(() => MySQLDatabaseEngine(dbUri, helpers)).toThrow()
-      helpers.getUserInfo = null as any
-      expect(() => MySQLDatabaseEngine(dbUri, helpers)).toThrow()
-    })
-  })
-})
+      test(`throws if getAdvisoryLockId is not function`, () => {
+        helpers.getAdvisoryLockId = '' as any
+        expect(() => MySQLDatabaseEngine(dbUri, helpers)).toThrow()
+        helpers.getAdvisoryLockId = null as any
+        expect(() => MySQLDatabaseEngine(dbUri, helpers)).toThrow()
+      })
 
-describe('methods: {open,close}', () => {
-  let connection: Connection
+      test(`throws if getUserInfo is missing`, () => {
+        delete helpers.getUserInfo
+        expect(() => MySQLDatabaseEngine(dbUri, helpers)).toThrow()
+      })
 
-  let engine: ReturnType<typeof MySQLDatabaseEngine>
-
-  beforeAll(async () => {
-    const { databaseConfig } = getConfig(uri)
-
-    connection = createConnection(databaseConfig)
-
-    await runQuery(connection, 'DROP TABLE IF EXISTS ??;', [tableName])
-  })
-
-  afterAll(() => {
-    connection.destroy()
-  })
-
-  beforeEach(() => {
-    engine = MySQLDatabaseEngine(uri, {
-      baseVersion,
-      getAdvisoryLockId,
-      getUserInfo,
+      test(`throws if getUserInfo is not function`, () => {
+        helpers.getUserInfo = '' as any
+        expect(() => MySQLDatabaseEngine(dbUri, helpers)).toThrow()
+        helpers.getUserInfo = null as any
+        expect(() => MySQLDatabaseEngine(dbUri, helpers)).toThrow()
+      })
     })
   })
 
-  let recordColumnCount: number
+  describe('methods: {open,close}', () => {
+    let connection: Connection
 
-  test('can open & close (first run)', async () => {
-    recordColumnCount = await getTableColumnCount(
-      connection,
-      tableName,
-      'synor'
-    )
+    let engine: ReturnType<typeof MySQLDatabaseEngine>
 
-    expect(recordColumnCount).toBe(0)
+    beforeAll(async () => {
+      const { databaseConfig } = getConfig(uri)
 
-    await expect(engine.open()).resolves.toBeUndefined()
+      connection = createConnection(databaseConfig)
 
-    recordColumnCount = await getTableColumnCount(
-      connection,
-      tableName,
-      'synor'
-    )
-
-    expect(recordColumnCount).toBeGreaterThan(0)
-
-    await expect(engine.close()).resolves.toBeUndefined()
-  })
-
-  test('can open & close (after first run)', async () => {
-    await expect(
-      getTableColumnCount(connection, tableName, databaseName)
-    ).resolves.toBe(recordColumnCount)
-
-    await expect(engine.open()).resolves.toBeUndefined()
-
-    await expect(
-      getTableColumnCount(connection, tableName, databaseName)
-    ).resolves.toBe(recordColumnCount)
-
-    await expect(engine.close()).resolves.toBeUndefined()
-  })
-})
-
-describe('methods: {lock,unlock}', () => {
-  test('can lock & unlock', async () => {
-    const engine = MySQLDatabaseEngine(uri, {
-      baseVersion,
-      getAdvisoryLockId,
-      getUserInfo,
+      await runQuery(connection, 'DROP TABLE IF EXISTS ??;', [tableName])
     })
 
-    await engine.open()
-
-    await expect(engine.lock()).resolves.toBeUndefined()
-
-    await expect(engine.unlock()).resolves.toBeUndefined()
-
-    await engine.close()
-  })
-
-  test.skip('can not get multiple lock at once', async () => {
-    const engineOne = MySQLDatabaseEngine(uri, {
-      baseVersion,
-      getAdvisoryLockId,
-      getUserInfo,
+    afterAll(() => {
+      connection.destroy()
     })
 
-    const engineTwo = MySQLDatabaseEngine(uri, {
-      baseVersion,
-      getAdvisoryLockId,
-      getUserInfo,
+    beforeEach(() => {
+      engine = MySQLDatabaseEngine(uri, {
+        baseVersion,
+        getAdvisoryLockId,
+        getUserInfo,
+      })
     })
 
-    const callOrder: Array<'lock-1' | 'unlock-1' | 'lock-2' | 'unlock-2'> = []
+    let recordColumnCount: number
 
-    await Promise.all([engineOne.open(), engineTwo.open()])
-
-    await engineOne.lock().then(() => {
-      callOrder.push('lock-1')
-    })
-
-    await Promise.all([
-      engineTwo.lock().then(() => {
-        callOrder.push('lock-2')
-      }),
-      engineOne.unlock().then(() => {
-        callOrder.push('unlock-1')
-      }),
-    ])
-
-    await engineTwo.unlock().then(() => {
-      callOrder.push('unlock-2')
-    })
-
-    expect(callOrder).toEqual(['lock-1', 'unlock-1', 'lock-2', 'unlock-2'])
-
-    await Promise.all([engineOne.close(), engineTwo.close()])
-  })
-
-  test('lock throws if failed to get', async () => {
-    const queries = jest.requireActual('./queries')
-
-    jest.spyOn(queries, 'getQueryStore').mockImplementationOnce((...args) => {
-      const queryStore = queries.getQueryStore(...args)
-      queryStore.getLock = () => Promise.resolve(0)
-      return queryStore
-    })
-
-    const engine = MySQLDatabaseEngine(uri, {
-      baseVersion,
-      getAdvisoryLockId,
-      getUserInfo,
-    })
-
-    await engine.open()
-
-    await expect(engine.lock()).rejects.toThrow()
-
-    await engine.close()
-  })
-
-  test('unlock throws if not locked', async () => {
-    const engine = MySQLDatabaseEngine(uri, {
-      baseVersion,
-      getAdvisoryLockId,
-      getUserInfo,
-    })
-
-    await engine.open()
-
-    await expect(engine.unlock()).rejects.toThrow()
-
-    await engine.close()
-  })
-})
-
-describe('methods', () => {
-  let connection: Connection
-
-  let engine: ReturnType<typeof MySQLDatabaseEngine>
-
-  const OriginalDate = Date
-
-  beforeAll(async () => {
-    global.Date = class extends OriginalDate {
-      constructor() {
-        super('2020-01-01T00:00:00.000Z')
-      }
-    } as typeof global.Date
-
-    const { databaseConfig } = getConfig(uri)
-
-    connection = createConnection(databaseConfig)
-
-    await runQuery(connection, 'DROP TABLE IF EXISTS ??;', [tableName])
-  })
-
-  afterAll(() => {
-    connection.destroy()
-
-    global.Date = OriginalDate
-  })
-
-  beforeEach(async () => {
-    engine = MySQLDatabaseEngine(uri, {
-      baseVersion,
-      getAdvisoryLockId,
-      getUserInfo,
-    })
-
-    await engine.open()
-  })
-
-  afterEach(async () => {
-    await engine.close()
-  })
-
-  test('drop', async () => {
-    await expect(
-      getTableColumnCount(connection, tableName, databaseName)
-    ).resolves.toBeGreaterThan(0)
-    await expect(engine.drop()).resolves.toBeUndefined()
-    await expect(
-      getTableColumnCount(connection, tableName, databaseName)
-    ).resolves.toBe(0)
-  })
-
-  test('run (with body)', async () => {
-    await expect(engine.run(migrationSource['01.do'])).resolves.toBeUndefined()
-    await expect(engine.run(migrationSource['01.undo'])).rejects.toThrow()
-
-    await expect(
-      runQuery(connection, `SELECT * FROM ??;`, [tableName])
-    ).resolves.toMatchSnapshot()
-
-    await engine.drop()
-  })
-
-  test('run (with run)', async () => {
-    await expect(engine.run(migrationSource['02.do'])).resolves.toBeUndefined()
-
-    await expect(
-      runQuery(connection, `SELECT * FROM ??;`, [tableName])
-    ).resolves.toMatchSnapshot()
-
-    await engine.drop()
-  })
-
-  test('repair', async () => {
-    await expect(engine.run(migrationSource['01.do'])).resolves.toBeUndefined()
-    await expect(engine.run(migrationSource['01.undo'])).rejects.toThrow()
-
-    const [record] = await runQuery<any[]>(
-      connection,
-      `SELECT id FROM ?? WHERE version = ? AND type = ?;`,
-      [
+    test('can open & close (first run)', async () => {
+      recordColumnCount = await getTableColumnCount(
+        connection,
         tableName,
-        migrationSource['01.do'].version,
-        migrationSource['01.do'].type,
-      ]
-    )
+        'synor'
+      )
 
-    await expect(
-      engine.repair([
-        { id: record.id, hash: `${migrationSource['01.do'].hash}-repaired` },
-      ])
-    ).resolves.toBeUndefined()
+      expect(recordColumnCount).toBe(0)
 
-    await expect(
-      runQuery(connection, `SELECT * FROM ??;`, [tableName])
-    ).resolves.toMatchSnapshot()
+      await expect(engine.open()).resolves.toBeUndefined()
 
-    await engine.drop()
+      recordColumnCount = await getTableColumnCount(
+        connection,
+        tableName,
+        'synor'
+      )
+
+      expect(recordColumnCount).toBeGreaterThan(0)
+
+      await expect(engine.close()).resolves.toBeUndefined()
+    })
+
+    test('can open & close (after first run)', async () => {
+      await expect(
+        getTableColumnCount(connection, tableName, databaseName)
+      ).resolves.toBe(recordColumnCount)
+
+      await expect(engine.open()).resolves.toBeUndefined()
+
+      await expect(
+        getTableColumnCount(connection, tableName, databaseName)
+      ).resolves.toBe(recordColumnCount)
+
+      await expect(engine.close()).resolves.toBeUndefined()
+    })
   })
 
-  test('records', async () => {
-    await expect(engine.run(migrationSource['01.do'])).resolves.toBeUndefined()
-    await expect(engine.run(migrationSource['01.undo'])).rejects.toThrow()
+  describe('methods: {lock,unlock}', () => {
+    test('can lock & unlock', async () => {
+      const engine = MySQLDatabaseEngine(uri, {
+        baseVersion,
+        getAdvisoryLockId,
+        getUserInfo,
+      })
 
-    await expect(engine.records()).resolves.toMatchSnapshot()
+      await engine.open()
 
-    await engine.drop()
+      await expect(engine.lock()).resolves.toBeUndefined()
+
+      await expect(engine.unlock()).resolves.toBeUndefined()
+
+      await engine.close()
+    })
+
+    test.skip('can not get multiple lock at once', async () => {
+      const engineOne = MySQLDatabaseEngine(uri, {
+        baseVersion,
+        getAdvisoryLockId,
+        getUserInfo,
+      })
+
+      const engineTwo = MySQLDatabaseEngine(uri, {
+        baseVersion,
+        getAdvisoryLockId,
+        getUserInfo,
+      })
+
+      const callOrder: Array<'lock-1' | 'unlock-1' | 'lock-2' | 'unlock-2'> = []
+
+      await Promise.all([engineOne.open(), engineTwo.open()])
+
+      await engineOne.lock().then(() => {
+        callOrder.push('lock-1')
+      })
+
+      await Promise.all([
+        engineTwo.lock().then(() => {
+          callOrder.push('lock-2')
+        }),
+        engineOne.unlock().then(() => {
+          callOrder.push('unlock-1')
+        }),
+      ])
+
+      await engineTwo.unlock().then(() => {
+        callOrder.push('unlock-2')
+      })
+
+      expect(callOrder).toEqual(['lock-1', 'unlock-1', 'lock-2', 'unlock-2'])
+
+      await Promise.all([engineOne.close(), engineTwo.close()])
+    })
+
+    test('lock throws if failed to get', async () => {
+      const queries = jest.requireActual('./queries')
+
+      jest.spyOn(queries, 'getQueryStore').mockImplementationOnce((...args) => {
+        const queryStore = queries.getQueryStore(...args)
+        queryStore.getLock = () => Promise.resolve(0)
+        return queryStore
+      })
+
+      const engine = MySQLDatabaseEngine(uri, {
+        baseVersion,
+        getAdvisoryLockId,
+        getUserInfo,
+      })
+
+      await engine.open()
+
+      await expect(engine.lock()).rejects.toThrow()
+
+      await engine.close()
+    })
+
+    test('unlock throws if not locked', async () => {
+      const engine = MySQLDatabaseEngine(uri, {
+        baseVersion,
+        getAdvisoryLockId,
+        getUserInfo,
+      })
+
+      await engine.open()
+
+      await expect(engine.unlock()).rejects.toThrow()
+
+      await engine.close()
+    })
+  })
+
+  describe('methods', () => {
+    let connection: Connection
+
+    let engine: ReturnType<typeof MySQLDatabaseEngine>
+
+    const OriginalDate = Date
+
+    beforeAll(async () => {
+      global.Date = class extends OriginalDate {
+        constructor() {
+          super('2020-01-01T00:00:00.000Z')
+        }
+      } as typeof global.Date
+
+      const { databaseConfig } = getConfig(uri)
+
+      connection = createConnection(databaseConfig)
+
+      await runQuery(connection, 'DROP TABLE IF EXISTS ??;', [tableName])
+    })
+
+    afterAll(() => {
+      connection.destroy()
+
+      global.Date = OriginalDate
+    })
+
+    beforeEach(async () => {
+      engine = MySQLDatabaseEngine(uri, {
+        baseVersion,
+        getAdvisoryLockId,
+        getUserInfo,
+      })
+
+      await engine.open()
+    })
+
+    afterEach(async () => {
+      await engine.close()
+    })
+
+    test('drop', async () => {
+      await expect(
+        getTableColumnCount(connection, tableName, databaseName)
+      ).resolves.toBeGreaterThan(0)
+      await expect(engine.drop()).resolves.toBeUndefined()
+      await expect(
+        getTableColumnCount(connection, tableName, databaseName)
+      ).resolves.toBe(0)
+    })
+
+    test('run (with body)', async () => {
+      await expect(
+        engine.run(migrationSource['01.do'])
+      ).resolves.toBeUndefined()
+      await expect(engine.run(migrationSource['01.undo'])).rejects.toThrow()
+
+      await expect(
+        runQuery(connection, `SELECT * FROM ??;`, [tableName])
+      ).resolves.toMatchSnapshot()
+
+      await engine.drop()
+    })
+
+    test('run (with run)', async () => {
+      await expect(
+        engine.run(migrationSource['02.do'])
+      ).resolves.toBeUndefined()
+
+      await expect(
+        runQuery(connection, `SELECT * FROM ??;`, [tableName])
+      ).resolves.toMatchSnapshot()
+
+      await engine.drop()
+    })
+
+    test('repair', async () => {
+      await expect(
+        engine.run(migrationSource['01.do'])
+      ).resolves.toBeUndefined()
+      await expect(engine.run(migrationSource['01.undo'])).rejects.toThrow()
+
+      const [record] = await runQuery<any[]>(
+        connection,
+        `SELECT id FROM ?? WHERE version = ? AND type = ?;`,
+        [
+          tableName,
+          migrationSource['01.do'].version,
+          migrationSource['01.do'].type,
+        ]
+      )
+
+      await expect(
+        engine.repair([
+          { id: record.id, hash: `${migrationSource['01.do'].hash}-repaired` },
+        ])
+      ).resolves.toBeUndefined()
+
+      await expect(
+        runQuery(connection, `SELECT * FROM ??;`, [tableName])
+      ).resolves.toMatchSnapshot()
+
+      await engine.drop()
+    })
+
+    test('records', async () => {
+      await expect(
+        engine.run(migrationSource['01.do'])
+      ).resolves.toBeUndefined()
+      await expect(engine.run(migrationSource['01.undo'])).rejects.toThrow()
+
+      await expect(engine.records()).resolves.toMatchSnapshot()
+
+      await engine.drop()
+    })
   })
 })
--- packages/database-mysql/src/utils/run-query.test.ts
+++ packages/database-mysql/src/utils/run-query.test.ts
@@ -3,30 +3,35 @@ import { runQuery } from './run-query'
 
 type Connection = import('mysql2').Connection
 
-const uri = 'mysql://root:root@127.0.0.1:3306/synor'
+const uris = {
+  mysql5: `mysql://root:root@127.0.0.1:33065/synor`,
+  mysql8: `mysql://root:root@127.0.0.1:33068/synor`,
+}
 
-describe('utils:runQuery', () => {
-  let connection: Connection
+describe.each(Object.entries(uris))('for %s', (_, uri) => {
+  describe('utils:runQuery', () => {
+    let connection: Connection
 
-  beforeAll(() => {
-    connection = createConnection(uri)
-  })
+    beforeAll(() => {
+      connection = createConnection(uri)
+    })
 
-  afterAll(() => {
-    connection.destroy()
-  })
+    afterAll(() => {
+      connection.destroy()
+    })
 
-  test('can execute query', async () => {
-    await expect(runQuery(connection, 'SELECT 1;')).resolves.toMatchSnapshot()
-  })
+    test('can execute query', async () => {
+      await expect(runQuery(connection, 'SELECT 1;')).resolves.toMatchSnapshot()
+    })
 
-  test('can substitute values in query', async () => {
-    await expect(
-      runQuery(connection, 'SELECT ?;', [1])
-    ).resolves.toMatchSnapshot()
-  })
+    test('can substitute values in query', async () => {
+      await expect(
+        runQuery(connection, 'SELECT ?;', [1])
+      ).resolves.toMatchSnapshot()
+    })
 
-  test('throws if malformed query', async () => {
-    await expect(runQuery(connection, 'SELEC 1;')).rejects.toThrow()
+    test('throws if malformed query', async () => {
+      await expect(runQuery(connection, 'SELEC 1;')).rejects.toThrow()
+    })
   })
 })
